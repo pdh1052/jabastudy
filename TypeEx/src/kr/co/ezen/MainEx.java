2b8dd0735e6369d0f8bccb1f82318ef6cd8414c1
pdh1052 <pdh10512@naver.com>
2020-10-16 17:49:10 +0900

    10.16 study

---
+++ TypeEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,27 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		boolean a = true;
+		char aaa = 'r';
+		byte aa = 127;
+		short aaaa= 200;
+		int b = 2100000000;
+		long bb = 300000000000L;
+		float bbb = 3.14f;
+		double bbbb = 3.14195;
+		String msg = "a";
+		System.out.println(msg);
+		
+		
+		
+		
+				
+		
+		
+		
+				
+		
+	}
+
+}
---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,21 @@
+package kr.co.ezen;
+public class MainEx {
+public static void main(String[] args) {
+String msg = "where";
+String msg2 = "Hi";
+String msg3 = "Good";
+String msg4 = "Morning";
+
+	System.out.println(msg);
+	System.out.println(msg2);
+	System.out.println(msg3);
+	System.out.println(msg4);
+	System.out.println(msg);
+	System.out.println(msg3);
+	System.out.println(msg2);
+	System.out.println(msg);
+	System.out.println(msg);
+	System.out.println(msg2);
+	
+}
+}
