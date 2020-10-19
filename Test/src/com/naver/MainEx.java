ebc6566728dd91f2db781fb0c9c1bb1625b40f1a
pdh1052 <pdh10512@naver.com>
2020-10-19 17:51:04 +0900

    10월 19일 수업

---
+++ Test/src/com/naver/MainEx.java
@@ -0,0 +1,20 @@
+package com.naver;
+
+public class MainEx {
+	public static void main(String[] args) {
+		int a = 10;//변수 a를 선언하면서 10으로 초기화.
+		System.out.println(a);
+		System.out.println(a+1);
+		
+		System.out.println(a*10);
+		
+		
+		a = 100;
+		
+		System.out.println(a);
+		
+	int c = 0;
+	c = 44;
+	}
+
+}
---
+++ Test/src/kr/co/ezen/Dog.java
@@ -0,0 +1,17 @@
+package kr.co.ezen;
+
+public class Dog {
+	String b = new String("hello");
+	Object c;
+	boolean d = false;
+	
+	
+	
+	
+
+	public Dog() {
+		int a = 55;
+
+	}
+
+}
