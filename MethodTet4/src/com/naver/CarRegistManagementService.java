4593b4f9862932dd9fd601748cdda7848f266e72
pdh1052 <pdh10512@naver.com>
2020-10-22 17:26:29 +0900

    10월 22일 수업

---
+++ MethodTet4/src/com/naver/CarRegistManagementService.java
@@ -0,0 +1,40 @@
+package com.naver;
+
+public class CarRegistManagementService {
+	
+	private String carName;
+	
+	public CarRegistManagementService() {
+		carName = "¼Ò³ªÅ¸";
+		
+		
+	}
+	
+	
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+
+	
+	
+	public int getCountCarName(String carName) {
+		
+		int size = carName.length();
+		
+		return size;
+				
+	}
+	public int add(int a, int b) {
+		
+		return a+b;
+		
+		
+	}
+	
+	
+}
