9091548b7dbb33f78423303345a948891ca4a51c
pdh1052 <pdh10512@naver.com>
2020-10-26 17:37:10 +0900

    (10.26일 수업)

---
+++ InstanceEx/src/kr/co/ezen/Dog.java
@@ -0,0 +1,50 @@
+package kr.co.ezen;
+
+import java.util.Date;
+
+public class Dog {
+	
+	public Dog() {
+		
+		
+	}
+	
+	private int did;
+	private String name;
+	private Date birth;
+	
+	
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public Dog(int did, String name, Date birth) {
+		super();
+		this.did = did;
+		this.name = name;
+		this.birth = birth;
+	}
+
+	public String sleep(String where) {
+		
+		return where + "ø°º≠ ¿‹¥Ÿ.";
+		
+		
+	}
+}
